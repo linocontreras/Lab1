6a979dd48283e7c0cafae31961e9ff670680e9dd
Lino Contreras <linocontreras@outlook.com>
2018-08-16 19:45:10 -0500

    Initial commit

---
+++ app/src/main/java/com/linocontreras/lab1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.linocontreras.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Random r;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        r = new Random();
+    }
+
+    public void buttonOnClick(View v){
+        Button b = ((Button)v);
+        b.setText("Lalalalala");
+        b.setAllCaps(false);
+        b.setBackgroundColor(r.nextInt());
+        
+    }
+}
